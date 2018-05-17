2cf10a74cb278efe4f4d758a866f256088b3d2c7
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2018-05-17 10:31:39 +0100

    Copyright 2018 HM Revenue & Customs

--- app/uk/gov/hmrc/offpayroll/models/AnalyticsModel.scala
+++ app/uk/gov/hmrc/offpayroll/models/AnalyticsModel.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -118,7 +118,3 @@ case class InterviewSearchResponse(compressedInterview: String, route: String, d
                      partParcelContactWithEngagerCustomer: String,  partParcelWorkerRepresentsEngagerBusiness: String)
 
 case class AnalyticsResponse(interviews: List[InterviewSearchResponse])
-
-
-
-
