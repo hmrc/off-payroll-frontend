1e51edb9c8a723619853efe8721a00be06dcecdd
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2019-04-11 11:24:41 +0100

    added parser tests

--- app/connectors/DecisionHttpParser.scala
+++ app/connectors/DecisionHttpParser.scala
@@ -1,23 +1,24 @@
 /*
-* Copyright 2019 HM Revenue & Customs
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package connectors
 
 import models.{DecisionResponse, ErrorResponse}
 import play.api.Logger
-import play.mvc.Http.Status
+import play.mvc.Http.Status.{OK, INTERNAL_SERVER_ERROR}
 import uk.gov.hmrc.http.{HttpReads, HttpResponse}
 
 object DecisionHttpParser {
@@ -27,18 +28,18 @@ object DecisionHttpParser {
     override def read(method: String, url: String, response: HttpResponse): Either[ErrorResponse, DecisionResponse] = {
 
       response.status match {
-        case Status.OK => {
+        case OK => {
           Logger.debug("Received an OK response from decide API")
           response.json.validate[DecisionResponse].fold(
             invalid => {
-              Logger.debug(s"Received Invalid Json from decision connector - $invalid")
-              Left(ErrorResponse(Status.INTERNAL_SERVER_ERROR, "Invalid Json received from decision connector"))
+              Logger.warn(s"Received Invalid Json from decision connector - $invalid")
+              Left(ErrorResponse(INTERNAL_SERVER_ERROR, "Invalid Json received from decision connector"))
             },
             decisionResponse => Right(decisionResponse)
           )
         }
         case status =>
-          Logger.warn(s"Unexpected response from decide API - Status $status Response: ${response.body}")
+          Logger.error(s"Unexpected response from decide API - Status $status Response: ${response.body}")
           Left(ErrorResponse(status, s"Unexpected Response. Response: ${response.body}"))
       }
     }
---
+++ test/connectors/DecisionHttpParserSpec.scala
@@ -0,0 +1,89 @@
+/*
+* Copyright 2019 HM Revenue & Customs
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package connectors
+
+import models.ResultEnum.SELF_EMPLOYED
+import models.WeightedAnswerEnum._
+import models._
+import play.api.libs.json.{JsValue, Json}
+import uk.gov.hmrc.http.HttpResponse
+import uk.gov.hmrc.play.test.UnitSpec
+
+class DecisionHttpParserSpec extends UnitSpec {
+
+  val json: JsValue = Json.parse(
+    """
+      |{
+      |  "version": "1.5.0-final",
+      |  "correlationID": "12345",
+      |  "score": {
+      |    "setup": "CONTINUE",
+      |    "exit": "CONTINUE",
+      |    "personalService": "MEDIUM",
+      |    "control": "MEDIUM",
+      |    "financialRisk": "NotValidUseCase",
+      |    "partAndParcel": "NotValidUseCase"
+      |  },
+      |  "result": "Self-Employed"
+      |}
+    """.stripMargin)
+
+  val invalidJson: JsValue = Json.parse(
+    """
+      |{
+      |  "version": "1.5.0-final",
+      |  "correlationI": "12345",
+      |  "scoe": {
+      |    "setup": "CONTINUE",
+      |    "exit": "CONTINUE",
+      |    "personalService": "MEDIUM",
+      |    "control": "MEDIUM",
+      |    "financialRisk": "NotValidUseCase",
+      |    "partAndParcel": "NotValidUseCase"
+      |  },
+      |  "result": "Self-Employed"
+      |}
+    """.stripMargin)
+
+  import play.mvc.Http.Status._
+
+
+  "Http parser" should {
+    "parse json to model" in {
+
+      DecisionHttpParser.DecisionReads.read("decision","decision", HttpResponse(OK, Some(json))) shouldBe
+        Right(DecisionResponse("1.5.0-final", "12345",
+          Score(Some(SetupEnum.CONTINUE), Some(ExitEnum.CONTINUE), Some(MEDIUM),Some(MEDIUM),Some(NOT_VALID_USE_CASE),Some(NOT_VALID_USE_CASE)),
+          SELF_EMPLOYED
+        ))
+    }
+
+    "handle invalid json" in {
+      DecisionHttpParser.DecisionReads.read("decision","decision", HttpResponse(OK, Some(invalidJson))) shouldBe
+        Left(ErrorResponse(INTERNAL_SERVER_ERROR, "Invalid Json received from decision connector"))
+    }
+
+    "handle errors" in {
+      DecisionHttpParser.DecisionReads.read("decision","decision", HttpResponse(BAD_REQUEST, None)) shouldBe
+        Left(ErrorResponse(BAD_REQUEST,"Unexpected Response. Response: null"))
+
+      DecisionHttpParser.DecisionReads.read("decision","decision", HttpResponse(BAD_REQUEST, Some(Json.parse("""{"error":"bad request"}""")))) shouldBe
+        Left(ErrorResponse(BAD_REQUEST,"""Unexpected Response. Response: {
+                                         |  "error" : "bad request"
+                                         |}""".stripMargin))
+    }
+  }
+}
