4fb461db4daae2a4b632e85fa1fc442a5b17aa3d
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2019-05-14 08:54:37 +0100

    updated run file

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sbt -Dlogger.resource=logback-test.xml -Dplay.http.router=testOnlyDoNotUseInAppConf.Routes run
+sbt -Dlogger.resource=logback-test.xml -Dplay.http.router=testOnlyDoNotUseInAppConf.Routes "run 9843"
