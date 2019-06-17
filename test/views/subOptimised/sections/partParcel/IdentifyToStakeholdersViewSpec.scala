260e8fcc3eda19a3677555a4a89b89c034719e7b
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2019-06-17 12:37:13 +0100

    fix bullets

--- test/views/subOptimised/sections/partParcel/IdentifyToStakeholdersViewSpec.scala
+++ test/views/subOptimised/sections/partParcel/IdentifyToStakeholdersViewSpec.scala
@@ -132,17 +132,17 @@ class IdentifyToStakeholdersViewSpec extends ViewBehaviours {
     }
 
     //TODO Randomly fails
-//    for(option <- IdentifyToStakeholders.options) {
-//      s"rendered with a value of '${option.value}'" must {
-//        s"have the '${option.value}' radio button selected" in {
-//          val doc = asDocument(createViewUsingForm(form.bind(Map("value" -> s"${option.value}"))))
-//          assertContainsRadioButton(doc, option.id, "value", option.value, true)
-//
-//          for(unselectedOption <- IdentifyToStakeholders.options.filterNot(o => o == option)) {
-//            assertContainsRadioButton(doc, unselectedOption.id, "value", unselectedOption.value, false)
-//          }
-//        }
-//      }
-//    }
+    for(option <- IdentifyToStakeholders.options) {
+      s"rendered with a value of '${option.value}'" must {
+        s"have the '${option.value}' radio button selected" in {
+          val doc = asDocument(createViewUsingForm(form.bind(Map("value" -> s"${option.value}"))))
+          assertContainsRadioButton(doc, option.id, "value", option.value, true)
+
+          for(unselectedOption <- IdentifyToStakeholders.options.filterNot(o => o == option)) {
+            assertContainsRadioButton(doc, unselectedOption.id, "value", unselectedOption.value, false)
+          }
+        }
+      }
+    }
   }
 }
