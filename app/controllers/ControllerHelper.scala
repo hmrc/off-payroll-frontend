a64f4418e999aa30846b644f1353d9b1db168493
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2019-06-17 11:35:28 +0100

    turned off scalastyle

--- app/controllers/ControllerHelper.scala
+++ app/controllers/ControllerHelper.scala
@@ -41,7 +41,6 @@ class ControllerHelper @Inject()(compareAnswerService: CompareAnswerService,
                                  decisionConnector: DecisionConnector,
                                  optimisedDecisionService: OptimisedDecisionService
                                 )(implicit val appConf: FrontendAppConfig) extends BaseController(controllerComponents) with FeatureSwitching {
-  //scalastyle:off
   def redirect[T](mode: Mode,
                   value: T,
                   page: QuestionPage[T],
