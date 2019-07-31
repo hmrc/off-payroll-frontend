eaa3a7a094d1fa9b0c4dd1fbac4e90e15434c1b7
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2019-07-31 16:48:35 +0100

    [CEST-500] > updated tests for CheckYourAnswersHelperSpec

--- test/utils/CheckYourAnswersHelperSpec.scala
+++ test/utils/CheckYourAnswersHelperSpec.scala
@@ -829,7 +829,7 @@ class CheckYourAnswersHelperSpec extends SpecBase with Enumerable.Implicits {
           new CheckYourAnswersHelper(cacheMap).rejectSubstitute(messages, workerRequest, frontendAppConfig) mustBe
             Some(AnswerRow(
               label = s"$Worker.optimised.$RejectSubstitutePage.checkYourAnswersLabel",
-              answer = "site.no",
+              answer = "site.yes",
               answerIsMessageKey = true,
               changeUrl = Some(controllers.routes.PersonalServiceSectionChangeWarningController.onPageLoad(RejectSubstitutePage).url)
             ))
@@ -843,7 +843,7 @@ class CheckYourAnswersHelperSpec extends SpecBase with Enumerable.Implicits {
           new CheckYourAnswersHelper(cacheMap).rejectSubstitute(messages, hirerRequest, frontendAppConfig) mustBe
             Some(AnswerRow(
               label = s"$Hirer.optimised.$RejectSubstitutePage.checkYourAnswersLabel",
-              answer = "site.no",
+              answer = "site.yes",
               answerIsMessageKey = true,
               changeUrl = Some(controllers.routes.PersonalServiceSectionChangeWarningController.onPageLoad(RejectSubstitutePage).url)
             ))
@@ -857,7 +857,7 @@ class CheckYourAnswersHelperSpec extends SpecBase with Enumerable.Implicits {
           new CheckYourAnswersHelper(cacheMap).rejectSubstitute(messages, fakeRequest, frontendAppConfig) mustBe
             Some(AnswerRow(
               label = s"optimised.$RejectSubstitutePage.checkYourAnswersLabel",
-              answer = "site.no",
+              answer = "site.yes",
               answerIsMessageKey = true,
               changeUrl = Some(controllers.routes.PersonalServiceSectionChangeWarningController.onPageLoad(RejectSubstitutePage).url)
             ))
