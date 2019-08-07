52487021e43d163ec88481806c7155c2ed75c282
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2019-08-07 13:50:37 +0100

    CEST-IT added Index and about you IT tests

--- it/controllers/AboutYouControllerISpec.scala
+++ it/controllers/AboutYouControllerISpec.scala
@@ -21,11 +21,12 @@ class AboutYouControllerISpec extends IntegrationSpecBase with CreateRequestHelp
     }
 
 
-    "Return a 200 on successful get" in {
+    "Return a 200 on successful get and should be on relevant page" in {
 
       lazy val res = getSessionRequest("/reason-for-using-tool", cookies,true)
       whenReady(res) { result =>
          result.status shouldBe OK
+        result.body should include ("What do you want to find out?")
       }
 
     }
@@ -49,7 +50,6 @@ class AboutYouControllerISpec extends IntegrationSpecBase with CreateRequestHelp
         result.body should include ("What do you want to find out?")
 
       }
-
     }
 
     "Return a 200 on Successful post and move onto next page" in {
@@ -64,5 +64,63 @@ class AboutYouControllerISpec extends IntegrationSpecBase with CreateRequestHelp
     }
 
   }
+
+  s"Post or Get to /reason-for-using-tool/change" should {
+
+    "Get sessionheaders successfully" in {
+
+      lazy val sessionResult = getRequest("/disclaimer", true)
+
+      whenReady(sessionResult) { result =>
+        cookies = result.cookies
+      }
+
+    }
+
+
+    "Return a 200 on successful get and should be on relevant page" in {
+
+      lazy val res = getSessionRequest("/reason-for-using-tool/change", cookies,true)
+      whenReady(res) { result =>
+        result.status shouldBe OK
+        result.body should include ("What do you want to find out?")
+      }
+
+    }
+
+    "Return a 404 on a post to unused method" in {
+
+      lazy val res = optionsRequest("/reason-for-using-tool/change")
+
+      whenReady(res) { result =>
+        result.status shouldBe NOT_FOUND
+      }
+
+    }
+
+    "Return a 400 on unsuccessful post and stay on the same page" in {
+
+      lazy val res = postSessionRequest("/reason-for-using-tool/change",defaultValue, cookies)
+
+      whenReady(res) { result =>
+        result.status shouldBe BAD_REQUEST
+        result.body should include ("What do you want to find out?")
+
+      }
+    }
+
+    "Return a 200 on Successful post and move onto next page" in {
+
+      lazy val res = postSessionRequest("/reason-for-using-tool/change",aboutYouValue, cookies)
+
+      whenReady(res) { result =>
+        result.status shouldBe OK
+        result.body should include ("Are you trading through a limited company, partnership or unincorporated body?")
+      }
+
+    }
+
+  }
+
 
 }
---
+++ it/controllers/IndexControllerISpec.scala
@@ -0,0 +1,59 @@
+package controllers
+
+import helpers.{CreateRequestHelper, IntegrationSpecBase, TestData}
+import play.api.http.Status
+
+class IndexControllerISpec extends IntegrationSpecBase with CreateRequestHelper with Status with TestData{
+
+
+  s"Get /setup" should {
+
+    "Return a 200 on successful get" in {
+
+      lazy val res = getRequest("/setup", true)
+
+      whenReady(res) { result =>
+        result.status shouldBe OK
+      }
+
+    }
+
+    "Return a 404 on unsuccessful post" in {
+
+      lazy val res = optionsRequest("/setup")
+
+      whenReady(res) { result =>
+        result.status shouldBe NOT_FOUND
+      }
+
+    }
+
+
+  }
+
+  s"Get /" should {
+
+    "Return a 200 on successful get" in {
+
+      lazy val res = getRequest("/", true)
+
+      whenReady(res) { result =>
+        result.status shouldBe OK
+      }
+
+    }
+
+    "Return a 404 on unsuccessful post" in {
+
+      lazy val res = optionsRequest("/")
+
+      whenReady(res) { result =>
+        result.status shouldBe NOT_FOUND
+      }
+
+    }
+
+
+  }
+
+}
