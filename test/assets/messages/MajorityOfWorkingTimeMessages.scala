8ec5e247faf785958b4cc5e2716ad44ae0c5a5a6
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2019-08-08 10:09:25 +0100

    [CEST-552] Change isn't to is not

--- test/assets/messages/MajorityOfWorkingTimeMessages.scala
+++ test/assets/messages/MajorityOfWorkingTimeMessages.scala
@@ -22,14 +22,14 @@ object MajorityOfWorkingTimeMessages extends BaseMessages {
     val title = "Will this work take up the majority of your available working time?"
     val heading = "Will this work take up the majority of your available working time?"
     val subheading = "Worker’s contracts"
-    val p1 = "This includes preparation or any other time necessary to deliver the work, even if it isn’t referred to in the contract."
+    val p1 = "This includes preparation or any other time necessary to deliver the work, even if it is not referred to in the contract."
   }
 
   object Hirer {
     val title = "Will this work take up the majority of the worker’s available working time?"
     val heading = "Will this work take up the majority of the worker’s available working time?"
     val subheading = "Worker’s contracts"
-    val p1 = "This includes preparation or any other time necessary to deliver the work, even if it isn’t referred to in the contract."
+    val p1 = "This includes preparation or any other time necessary to deliver the work, even if it is not referred to in the contract."
   }
 
 }
