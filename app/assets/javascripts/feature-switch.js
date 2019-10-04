1662a7d03f7850c4c30c03132b4a3a9559f1d4b6
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2019-10-04 13:39:55 +0100

    CEST-659 remove BOOA switch

--- app/assets/javascripts/feature-switch.js
+++ app/assets/javascripts/feature-switch.js
@@ -2,12 +2,9 @@ $(document).ready(function() {
     $("#feature-switch\\.optimisedFlow").click(function() {
         if(this.checked) {
             $("#feature-switch\\.decisionServiceVersion-2\\.2").prop("disabled", false);
-            $("#feature-switch\\.businessOnOwnAccount").prop("disabled", false);
         } else {
             $("#feature-switch\\.decisionServiceVersion-1\\.5\\.0-final").prop("checked", true);
             $("#feature-switch\\.decisionServiceVersion-2\\.2").prop("disabled", true);
-            $("#feature-switch\\.businessOnOwnAccount").prop("disabled", true);
-            $("#feature-switch\\.businessOnOwnAccount").prop("checked", false);
         }
     });
 });
