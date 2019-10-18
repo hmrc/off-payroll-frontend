6b397e1eb9cc3a87dce41590371066922ebd86a1
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2019-10-18 13:50:58 +0100

    [CEST-727]: updated DecisionServiceVersion

--- app/assets/javascripts/feature-switch.js
+++ app/assets/javascripts/feature-switch.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
     $("#feature-switch\\.optimisedFlow").click(function() {
         if(this.checked) {
             $("#feature-switch\\.decisionServiceVersion-2\\.2").prop("disabled", false);
+            $("#feature-switch\\.decisionServiceVersion-2\\.4").prop("disabled", false);
         } else {
             $("#feature-switch\\.decisionServiceVersion-1\\.5\\.0-final").prop("checked", true);
             $("#feature-switch\\.decisionServiceVersion-2\\.2").prop("disabled", true);
+            $("#feature-switch\\.decisionServiceVersion-2\\.4").prop("disabled", true);
         }
     });
 });
