5d1a3ed683c22d26c34552d44ed38dcb099e0ffd
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2021-02-27 23:02:56 +0000

    DL-5034 dependency and plugin updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,22 +7,22 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.30.0-play-27",
-    "uk.gov.hmrc" %% "logback-json-logger" % "4.8.0",
-    "uk.gov.hmrc" %% "govuk-template" % "5.59.0-play-27",
-    "uk.gov.hmrc" %% "play-health" % "3.15.0-play-27",
-    "uk.gov.hmrc" %% "play-ui" % "8.15.0-play-27",
-    "uk.gov.hmrc" %% "http-caching-client" % "9.1.0-play-27",
-    "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.4.0-play-26",
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "3.0.0",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.31.0-play-27",
+    "uk.gov.hmrc" %% "logback-json-logger" % "4.9.0",
+    "uk.gov.hmrc" %% "govuk-template" % "5.63.0-play-27",
+    "uk.gov.hmrc" %% "play-health" % "3.16.0-play-27",
+    "uk.gov.hmrc" %% "play-ui" % "8.21.0-play-27",
+    "uk.gov.hmrc" %% "http-caching-client" % "9.2.0-play-27",
+    "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.6.0-play-26",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "3.4.0",
     "uk.gov.hmrc" %% "play-whitelist-filter" % "3.4.0-play-27",
     "com.typesafe.play" %% "play-json-joda" % "2.7.4"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "service-integration-test" % "0.12.0-play-27",
-    "uk.gov.hmrc" %% "reactivemongo-test" % "4.21.0-play-27",
-    "org.scalatest" %% "scalatest" % "3.3.0-SNAP2",
+    "uk.gov.hmrc" %% "service-integration-test" % "0.13.0-play-27",
+    "uk.gov.hmrc" %% "reactivemongo-test" % "4.22.0-play-27",
+    "org.scalatest" %% "scalatest" % "3.3.0-SNAP3",
     "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3",
     "org.scalatestplus" %% "scalatestplus-mockito" % "1.0.0-SNAP5",
     "org.scalatestplus" %% "scalacheck-1-14" % "3.2.2.0",
