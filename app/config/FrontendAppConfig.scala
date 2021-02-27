d88dee855f6358a16589ac8d63a48ac4f5d48046
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2021-02-27 21:38:40 +0000

    DL-5034 Add cookie consent and remove ga config

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2021 HM Revenue & Customs
  *
  */
 
@@ -22,8 +22,6 @@ class FrontendAppConfig @Inject() (environment: Environment, val servicesConfig:
   private lazy val contactHost = servicesConfig.getString("contact-frontend.host")
   private val contactFormServiceIdentifier = "off-payroll"
 
-  lazy val analyticsToken = servicesConfig.getString(s"google-analytics.token")
-  lazy val analyticsHost = servicesConfig.getString(s"google-analytics.host")
   lazy val reportAProblemPartialUrl = s"$contactHost/contact/problem_reports_ajax?service=$contactFormServiceIdentifier"
   lazy val reportAProblemNonJSUrl = s"$contactHost/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier"
 
@@ -44,10 +42,10 @@ class FrontendAppConfig @Inject() (environment: Environment, val servicesConfig:
     s"$contactHost/contact/beta-feedback-unauthenticated?service=$contactFormServiceIdentifier&backUrl=$requestPath"
   def reportAccessibilityIssueUrl(problemPageUri: String)(implicit request: Request[_]): String =
     s"$contactHost/contact/accessibility-unauthenticated?service=" +
-      s"${contactFormServiceIdentifier}&userAction=${SafeRedirectUrl(host + problemPageUri).encodedUrl}"
+      s"$contactFormServiceIdentifier&userAction=${SafeRedirectUrl(host + problemPageUri).encodedUrl}"
 
-  lazy val loginUrl = servicesConfig.getString("urls.login")
-  lazy val loginContinueUrl = servicesConfig.getString("urls.loginContinue")
+  lazy val loginUrl: String = servicesConfig.getString("urls.login")
+  lazy val loginContinueUrl: String = servicesConfig.getString("urls.loginContinue")
 
   lazy val host: String = servicesConfig.getString("host")
 
@@ -58,38 +56,38 @@ class FrontendAppConfig @Inject() (environment: Environment, val servicesConfig:
 
   lazy val mongoTtl: Int = servicesConfig.getInt("mongodb.timeToLiveInSeconds")
 
-  def decisionVersion = FeatureSwitching.getValue(DecisionServiceVersionFeature)(this)
+  def decisionVersion: String = FeatureSwitching.getValue(DecisionServiceVersionFeature)(this)
 
-  lazy val pdfGeneratorService = servicesConfig.baseUrl("pdf-generator-service")
-  lazy val assetsFrontendUrl = servicesConfig.getString("assets.url")
-  lazy val assetsFrontendVersion = servicesConfig.getString("assets.version")
+  lazy val pdfGeneratorService: String = servicesConfig.baseUrl("pdf-generator-service")
+  lazy val assetsFrontendUrl: String = servicesConfig.getString("assets.url")
+  lazy val assetsFrontendVersion: String = servicesConfig.getString("assets.version")
 
   lazy val timeoutPeriod: Int = servicesConfig.getInt("timeout.period")
   lazy val timeoutCountdown: Int = servicesConfig.getInt("timeout.countdown")
 
-  lazy val govUkStartPageUrl = servicesConfig.getString("urls.govUkStartPage")
-  lazy val employmentStatusManualUrl = servicesConfig.getString("urls.employmentStatusManual")
-  lazy val employmentStatusManualChapter5Url = servicesConfig.getString("urls.employmentStatusManualChapter5")
-  lazy val employmentStatusManualESM0527Url = servicesConfig.getString("urls.employmentStatusManualESM0527")
-  lazy val employmentStatusManualESM0521Url = servicesConfig.getString("urls.employmentStatusManualESM0521")
-  lazy val employmentStatusManualESM11160Url = servicesConfig.getString("urls.employmentStatusManualESM11160")
-  lazy val officeHolderUrl = servicesConfig.getString("urls.officeHolder")
-  lazy val understandingOffPayrollUrl = servicesConfig.getString("urls.understandingOffPayroll")
-  lazy val feePayerResponsibilitiesUrl = servicesConfig.getString("urls.feePayerResponsibilities")
-  lazy val payeForEmployersUrl = servicesConfig.getString("urls.payeForEmployers")
-  lazy val govukAccessibilityStatementUrl = servicesConfig.getString("urls.govukAccessibilityStatement")
-  lazy val abilityNetUrl = servicesConfig.getString("urls.abilityNet")
-  lazy val wcagUrl = servicesConfig.getString("urls.wcag")
-  lazy val eassUrl = servicesConfig.getString("urls.eass")
-  lazy val ecniUrl = servicesConfig.getString("urls.ecni")
-  lazy val hmrcAdditionalNeedsUrl = servicesConfig.getString("urls.hmrcAdditionalNeeds")
-  lazy val dacUrl = servicesConfig.getString("urls.dac")
+  lazy val govUkStartPageUrl: String = servicesConfig.getString("urls.govUkStartPage")
+  lazy val employmentStatusManualUrl: String = servicesConfig.getString("urls.employmentStatusManual")
+  lazy val employmentStatusManualChapter5Url: String = servicesConfig.getString("urls.employmentStatusManualChapter5")
+  lazy val employmentStatusManualESM0527Url: String = servicesConfig.getString("urls.employmentStatusManualESM0527")
+  lazy val employmentStatusManualESM0521Url: String = servicesConfig.getString("urls.employmentStatusManualESM0521")
+  lazy val employmentStatusManualESM11160Url: String = servicesConfig.getString("urls.employmentStatusManualESM11160")
+  lazy val officeHolderUrl: String = servicesConfig.getString("urls.officeHolder")
+  lazy val understandingOffPayrollUrl: String = servicesConfig.getString("urls.understandingOffPayroll")
+  lazy val feePayerResponsibilitiesUrl: String = servicesConfig.getString("urls.feePayerResponsibilities")
+  lazy val payeForEmployersUrl: String = servicesConfig.getString("urls.payeForEmployers")
+  lazy val govukAccessibilityStatementUrl: String = servicesConfig.getString("urls.govukAccessibilityStatement")
+  lazy val abilityNetUrl: String = servicesConfig.getString("urls.abilityNet")
+  lazy val wcagUrl: String = servicesConfig.getString("urls.wcag")
+  lazy val eassUrl: String = servicesConfig.getString("urls.eass")
+  lazy val ecniUrl: String = servicesConfig.getString("urls.ecni")
+  lazy val hmrcAdditionalNeedsUrl: String = servicesConfig.getString("urls.hmrcAdditionalNeeds")
+  lazy val dacUrl: String = servicesConfig.getString("urls.dac")
 
-  lazy val accessibilityStatementLastUpdatedDay = servicesConfig.getString("accessibilityStatementLastUpdatedDate.day")
-  lazy val accessibilityStatementLastUpdatedMonth = servicesConfig.getString("accessibilityStatementLastUpdatedDate.month")
-  lazy val accessibilityStatementLastUpdatedYear = servicesConfig.getString("accessibilityStatementLastUpdatedDate.year")
-  lazy val lastDacTestDay = servicesConfig.getString("lastDacTestDate.day")
-  lazy val lastDacTestMonth = servicesConfig.getString("lastDacTestDate.month")
-  lazy val lastDacTestYear = servicesConfig.getString("lastDacTestDate.year")
+  lazy val accessibilityStatementLastUpdatedDay: String = servicesConfig.getString("accessibilityStatementLastUpdatedDate.day")
+  lazy val accessibilityStatementLastUpdatedMonth: String = servicesConfig.getString("accessibilityStatementLastUpdatedDate.month")
+  lazy val accessibilityStatementLastUpdatedYear: String = servicesConfig.getString("accessibilityStatementLastUpdatedDate.year")
+  lazy val lastDacTestDay: String = servicesConfig.getString("lastDacTestDate.day")
+  lazy val lastDacTestMonth: String = servicesConfig.getString("lastDacTestDate.month")
+  lazy val lastDacTestYear: String = servicesConfig.getString("lastDacTestDate.year")
 
 }
