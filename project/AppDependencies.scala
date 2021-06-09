ddf95612849afb3f60bbf1146a60a199ebbde794
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2021-06-09 14:37:59 +0100

    DL-5298 Upgrade boostrap-play and disable webchat

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,7 +14,7 @@ object AppDependencies {
     "uk.gov.hmrc" %% "play-ui" % "9.1.0-play-27",
     "uk.gov.hmrc" %% "http-caching-client" % "9.2.0-play-27",
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.6.0-play-26",
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "3.4.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "5.2.0",
     "uk.gov.hmrc" %% "play-allowlist-filter" % "1.0.0-play-27",
     "com.typesafe.play" %% "play-json-joda" % "2.7.4",
     "uk.gov.hmrc" %% "digital-engagement-platform-chat" % "0.15.0-play-27"
