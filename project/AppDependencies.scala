0e35a43631894db0047666f3c1f7ac062dde3af6
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2021-06-09 14:33:09 +0100

    DL-5692 Webchat implementation

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -16,7 +16,8 @@ object AppDependencies {
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.6.0-play-26",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "3.4.0",
     "uk.gov.hmrc" %% "play-allowlist-filter" % "1.0.0-play-27",
-    "com.typesafe.play" %% "play-json-joda" % "2.7.4"
+    "com.typesafe.play" %% "play-json-joda" % "2.7.4",
+    "uk.gov.hmrc" %% "digital-engagement-platform-chat" % "0.15.0-play-27"
   )
 
   val test = Seq(
@@ -30,7 +31,7 @@ object AppDependencies {
     "org.jsoup" % "jsoup" % "1.13.1",
     "com.typesafe.play" %% "play-test" % PlayVersion.current,
     "org.mockito" % "mockito-all" % "2.0.2-beta",
-    "org.scalacheck" %% "scalacheck" % "1.14.3",
+    "org.scalacheck" %% "scalacheck" % "1.15.4",
     "org.scalamock" %% "scalamock-scalatest-support" % "3.6.0",
     "com.github.tomakehurst" % "wiremock-jre8" % wireMockVersion,
     "com.github.tomakehurst" % "wiremock-standalone" % wireMockVersion,
--- test/base/GuiceAppSpecBase.scala
+++ test/base/GuiceAppSpecBase.scala
@@ -10,11 +10,12 @@ import connectors.{DataCacheConnector, FakeDataCacheConnector}
 import handlers.ErrorHandler
 import org.scalatest.BeforeAndAfterEach
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.Application
+import play.api.{Application, inject}
 import play.api.i18n.{Messages, MessagesApi}
-import play.api.inject.bind
+import play.api.inject.{Injector, bind}
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.mvc.MessagesControllerComponents
+import uk.gov.hmrc.webchat.client.WebChatClient
 
 import scala.concurrent.ExecutionContext
 
@@ -22,10 +23,11 @@ import scala.concurrent.ExecutionContext
 trait GuiceAppSpecBase extends SpecBase with GuiceOneAppPerSuite with BeforeAndAfterEach {
 
   override lazy val app: Application = GuiceApplicationBuilder()
-    .overrides(bind[DataCacheConnector].to[FakeDataCacheConnector])
+    .overrides(bind[DataCacheConnector].to[FakeDataCacheConnector],
+      inject.bind[WebChatClient].toInstance(new uk.gov.hmrc.webchat.testhelpers.WebChatClientStub))
     .build()
 
-  lazy val injector = app.injector
+  lazy val injector: Injector = app.injector
 
   implicit lazy val ec: ExecutionContext = injector.instanceOf[ExecutionContext]
 
@@ -35,6 +37,6 @@ trait GuiceAppSpecBase extends SpecBase with GuiceOneAppPerSuite with BeforeAndA
   lazy val messagesApi: MessagesApi = injector.instanceOf[MessagesApi]
   implicit lazy val messages: Messages = messagesApi.preferred(Seq(lang))
 
-  val errorHandler = injector.instanceOf[ErrorHandler]
+  val errorHandler: ErrorHandler = injector.instanceOf[ErrorHandler]
 
 }
