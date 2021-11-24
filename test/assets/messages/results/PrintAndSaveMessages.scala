8cccc258dee8c5d7e86c88d253dddc2eb9283457
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2021-11-24 17:26:47 +0000

    DL-6646 update tests

--- test/assets/messages/results/PrintAndSaveMessages.scala
+++ test/assets/messages/results/PrintAndSaveMessages.scala
@@ -8,13 +8,9 @@ package assets.messages.results
 object PrintAndSaveMessages extends BaseResultMessages {
 
   val heading = "Keep a copy of your determination"
-  val printLink = "Print this document"
-  val printLinkAria = "Print a copy of your determination"
-  val savePdfLink = "Save as a PDF"
-  val savePdfLinkAria = "Save a copy of your determination as a PDF"
-  val exitLink = "exit"
-  val exitLinkAria = "Exit the check employment status for tax tool"
-  val startAgainLink = "start again"
-  val startAgainLinkAria = "Start the check employment status for tax tool again"
-  val p2 = "Once you have a copy of your result you can exit, or start again to check the employment status of some other work."
+  val printLink = "Print your determination"
+  val savePdfLink = "Save your determination as a PDF"
+  val exitLink = "exit the check employment status service"
+  val startAgainLink = "check the employment status of other work"
+  val p2 = "Once you have a copy of your determination you can:"
 }
