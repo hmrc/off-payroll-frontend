052d7cbe69ccaf0d24fadfefdbc6a83341240dd7
ddct-live-services <61273204+ddct-live-services@users.noreply.github.com>
2022-01-14 17:31:53 +0000

    DL-7050 copyright dependency and plugin updaates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,21 +3,21 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion._
 
-  val wireMockVersion = "2.31.0"
+  val wireMockVersion = "2.32.0"
   val wireMockStandAloneVersion = "2.27.2"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "simple-reactivemongo"             % "8.0.0-play-28",
     "uk.gov.hmrc"       %% "logback-json-logger"              % "5.1.0",
-    "uk.gov.hmrc"       %% "govuk-template"                   % "5.72.0-play-28",
-    "uk.gov.hmrc"       %% "play-ui"                          % "9.7.0-play-28",
+    "uk.gov.hmrc"       %% "govuk-template"                   % "5.74.0-play-28",
+    "uk.gov.hmrc"       %% "play-ui"                          % "9.8.0-play-28",
     "uk.gov.hmrc"       %% "http-caching-client"              % "9.5.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"    % "1.10.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"       % "5.16.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"       % "5.18.0",
     "uk.gov.hmrc"       %% "play-allowlist-filter"            % "1.0.0-play-28",
     "com.typesafe.play" %% "play-json-joda"                   % "2.9.2",
-    "uk.gov.hmrc"       %% "digital-engagement-platform-chat" % "0.22.0-play-28"
+    "uk.gov.hmrc"       %% "digital-engagement-platform-chat" % "0.24.0-play-28"
   )
 
   val test = Seq(
@@ -25,7 +25,7 @@ object AppDependencies {
     "uk.gov.hmrc"                  %% "reactivemongo-test"          % "5.0.0-play-28",
     "org.scalatest"                %% "scalatest"                   % "3.3.0-SNAP3",
     "org.scalatestplus.play"       %% "scalatestplus-play"          % "5.1.0",
-    "org.mockito"                  %  "mockito-core"                % "4.0.0",
+    "org.mockito"                  %  "mockito-core"                % "4.2.0",
     "org.scalatestplus"            %% "scalatestplus-mockito"       % "1.0.0-M2",
     "org.scalatestplus"            %% "scalacheck-1-15"             % "3.2.10.0",
     "org.pegdown"                  %  "pegdown"                     % "1.6.0",
@@ -33,10 +33,10 @@ object AppDependencies {
     "com.typesafe.play"            %% "play-test"                   % current,
     "org.scalacheck"               %% "scalacheck"                  % "1.15.4",
     "org.scalamock"                %% "scalamock-scalatest-support" % "3.6.0",
-    "uk.gov.hmrc"                  %% "bootstrap-test-play-28"      % "5.16.0",
+    "uk.gov.hmrc"                  %% "bootstrap-test-play-28"      % "5.18.0",
     "com.github.tomakehurst"       %  "wiremock-jre8"               % wireMockVersion,
     "com.github.tomakehurst"       %  "wiremock-standalone"         % wireMockStandAloneVersion,
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"        % "2.13.0"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"        % "2.13.1"
   ).map(_ % "test, it")
   
   def apply() = compile ++ test
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,7 +1,7 @@
 resolvers += "HMRC-open-artefacts-maven" at "https://open.artefacts.tax.service.gov.uk/maven2"
 resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts.tax.service.gov.uk/ivy2"))(Resolver.ivyStylePatterns)
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.5.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.6.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.1.0")
 
@@ -11,6 +11,6 @@ addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
 
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.9.2")
 
-addSbtPlugin("org.irundaia.sbt" % "sbt-sassify" % "1.4.13")
+addSbtPlugin("org.irundaia.sbt" % "sbt-sassify" % "1.5.1")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-digest" % "1.1.4")
